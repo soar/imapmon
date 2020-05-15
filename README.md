e3df293c31e1c2e441149f808b9266ccf6d3d7bc
Aleksey @soar Smyrnov <i@soar.name>
2020-05-15 21:19:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# IMAP Mon
+
+Tool for monitoring IMAP mailboxes and retransmitting received emails via alternative channels
+
+## Settings
+
+Some options can be set via an environment variable (real or [dotenv](https://github.com/theskumar/python-dotenv)) or a command line flag.
+
+| Environment variable name | Command line flag | Description |
+| --- | --- | --- |
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+from setuptools import setup, find_namespace_packages
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name='imapmon',
+    version=Path('version.txt').read_text().strip(),
+    packages=find_namespace_packages(include=['imapmon.*']),
+    include_package_data=True,
+    url='https://soar.name',
+    license='MIT',
+    author='soar',
+    author_email='i@soar.name',
+    description='Tool for monitoring IMAP mailboxes and retransmitting received emails via alternative channels',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'update = imapmon.scripts.imapmon:run'
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
